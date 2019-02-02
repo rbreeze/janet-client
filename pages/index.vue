5e626d27c7888a0259a88466b9a430cac5ba3a38
Maxwell Michelon <maxmichelon15@gmail.com>
2019-02-02 12:00:24 -0800

    Added nav bar and static table

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,47 @@
 <template>
   <section class="container">
     <div>
-      <logo />
+      <!-- <logo /> -->
       <h1 class="title">
-        janet-client
+        Janet-Client
       </h1>
       <h2 class="subtitle">
         Client for JANET
       </h2>
+      <div>
+        <h3 class="collection">
+          Collections:
+        </h3>
+      </div>
+      <!-- Create Table to create collections and items -->
+      <div>
+        <div v-for="(collection, i) in collections">
+          <div class="header row">
+            <div class="col">Name</div>
+            <div class="col">Value</div>
+            <div class="col">Used</div>
+            <div class="col">Delete</div>
+          </div>
+          <div class="row" v-for="(item, j) in collection">
+            <div class="col">
+              <input type="text" v-model="item.name" />
+            </div>
+            <div class="col">
+              <input type="text" v-model="item.value" />
+            </div>
+            <div class="col">
+              {{ item.used }}
+            </div>
+            <div class="col">
+              <fa icon="trash-alt" @click="deleteItem(i, j)"> </fa>
+            </div>
+          </div>
+        </div>
+
+        <b-table striped hover :items="items"></b-table>
+
+      </div>
+
       <div class="links">
         <a
           href="https://nuxtjs.org/"
@@ -19,6 +53,7 @@
           target="_blank"
           class="button--grey"
         >GitHub</a>
+
       </div>
     </div>
   </section>
@@ -27,11 +62,64 @@
 <script>
 import Logo from '~/components/Logo.vue'
 
+
+// export default {
+//   components: {
+//     Logo
+//   }
+// }
+
+// export default {
+//   data () {
+//     let data = [{
+//       name: "Collection A",
+//       items: [
+//         { name: "foo", value: "bar", used: false },
+//         { name: "bla", value: "daf", used: true }
+//       ]
+//     }, {
+//       name: "Collection B",
+//       items: [{ name: "baz", value: "qux", used: true }]
+//     }]
+//     return {
+//       collections: data
+//     }
+//   },
+//   methods: {
+//     deleteItem: function(i, j) {
+//       collections[i].splice(j, 1)
+//     }
+//   }
+// }
+
+const items = [
+  { isActive: true, age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
+  { isActive: false, age: 21, first_name: 'Larsen', last_name: 'Shaw' },
+  {
+    isActive: false,
+    age: 89,
+    first_name: 'Geneva',
+    last_name: 'Wilson',
+    _rowVariant: 'danger'
+  },
+  {
+    isActive: true,
+    age: 40,
+    first_name: 'Thor',
+    last_name: 'Macdonald',
+    _cellVariants: { isActive: 'success', age: 'info', first_name: 'warning' }
+  },
+  { isActive: false, age: 29, first_name: 'Dick', last_name: 'Dunlap' }
+]
+
 export default {
-  components: {
-    Logo
+  data () {
+    return {
+      items: items
+    }
   }
 }
+
 </script>
 
 <style>
@@ -60,6 +148,15 @@ export default {
   color: #526488;
   word-spacing: 5px;
   padding-bottom: 15px;
+}
+
+.collection {
+  font-weight: 300;
+  font-size: 42px;
+  color: #f02d99;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+  align-content: flex-start;
 }
 
 .links {
