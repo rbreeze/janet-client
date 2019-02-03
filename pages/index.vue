0b1969cf3f84c3148719ae7318acffb59c09d911
Maxwell Michelon <maxmichelon15@gmail.com>
2019-02-02 20:15:33 -0800

    style on index

--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,9 @@
 
 h1, h3 {
   text-align: center !important;
-} 
+}
 .aliases {
-  list-style: none; 
+  list-style: none;
 }
 .table {
   border: 1px solid grey;
@@ -21,7 +21,7 @@ h1, h3 {
 }
 
 .btn {
-  color: white !important; 
+  color: white !important;
   font-weight: 500;
   margin: 0 auto;
 }
@@ -29,6 +29,10 @@ h1, h3 {
 .btn-default {
   color: black !important;
   border: 1px solid grey;
+}
+.fullPad{
+  padding: 10px;
+  border: 5px grey solid;
 }
 </style>
 
@@ -40,25 +44,25 @@ h1, h3 {
 
     <h3 class="mb-3 collection"> Collections </h3>
 
-    <div class="table row w-50 mx-auto mt-3 p-3 mb-5"> 
-      <div class="header row pb-3 mb-3"> 
+    <div class="table-responseive fullPad">
+      <div class="header row pb-3 mb-3">
         <div class="col">Link</div>
         <div class="col">Name</div>
         <div class="col">Aliases</div>
         <div class="col"></div>
         <div class="col"></div>
       </div>
-      <div v-for="(collection, i) in collections"> 
+      <div v-for="(collection, i) in collections">
         <div class="row">
           <div class="col">
             <nuxt-link :to="'/collection/' + collection.name">{{ collection.name }}</nuxt-link>
           </div>
           <div class="col">
-            <input type="text" class="form-control" v-model="collection.name" /> 
+            <input type="text" class="form-control" v-model="collection.name" />
           </div>
           <div class="col">
-            <ul class="aliases"> 
-              <li v-for="alias in collection.aliases" class="mb-2"> 
+            <ul class="aliases">
+              <li v-for="alias in collection.aliases" class="mb-2">
                 <input type="text" class="form-control" v-model="alias.value" placeholder="alias" />
               </li>
               <li @click="addAlias(i)" class="btn btn-default">Add Alias</li>
@@ -90,13 +94,13 @@ export default {
     deleteCollection: async function(i, name) {
       await axios.delete('http://54.188.72.217/api/collection/' + name)
       this.collections.splice(i, 1)
-    }, 
+    },
     updateCollection: async function(i) {
       axios.put('http://54.188.72.217/api/collection', this.collections[i], (err, data) => {
         if (err) console.log(err)
         else console.log("Successfully updated")
       })
-    }, 
+    },
     addAlias: function(i) {
       this.collections[i].aliases.push({ value: "" })
     }
